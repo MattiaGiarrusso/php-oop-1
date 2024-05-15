69488a4635da2801b3004dac49c81caedaca8c40
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-05-15 15:00:05 +0200

    Feat: add fix db

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 require_once __DIR__. '/models/movie.php';
 
 $avengers = new Movie ('Avengers','https://pad.mymovies.it/filmclub/2009/03/107/locandina.jpg','Superheroes','May');
-$djangoUnchained = new Movie ('Django Unchained', 'https://pad.mymovies.it/filmclub/2011/05/035/locandina.jpg' ,'Western, Commedy','April');
+$djangoUnchained = new Movie ('Django Unchained', 'https://pad.mymovies.it/filmclub/2011/05/035/locandina.jpg','Western, Commedy','April');
 
 $movie = [
     $avengers,
